ecd733ef83e1ef04359f713281f56daa81badd37
Mihai Galos <mihaigalos@gmail.com>
2021-05-07 17:50:09 +0200

    Reformat

--- src/consensus/proof_of_stake.go
+++ src/consensus/proof_of_stake.go
@@ -1,9 +1,9 @@
 package main
 
 type PendingTransaction struct {
-	from string
-	to   string
-	amount uint64
+	from        string
+	to          string
+	amount      uint64
 	age_seconds uint64
 }
 
@@ -12,13 +12,13 @@ type PendingTransactions struct {
 	capacity uint64
 }
 
-func (transactions* PendingTransactions)selectTransaction() uint64{
-	max_stake_index:= uint64(0)
+func (transactions *PendingTransactions) selectTransaction() uint64 {
+	max_stake_index := uint64(0)
 	max_stake := uint64(0)
 	for i := uint64(0); i < transactions.capacity; i++ {
 		// age prevents big staked amounts from hogging the network
 		stake := transactions.entries[i].amount * transactions.entries[i].age_seconds
-		if max_stake < stake{
+		if max_stake < stake {
 			max_stake = stake
 			max_stake_index = i
 		}
